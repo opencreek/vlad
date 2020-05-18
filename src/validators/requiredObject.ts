73c0e4a059163aa2828047e5702f3cf44c508d04
Leon Strauss <me@lionc.de>
2020-05-18 11:43:15 +0200

    :bug: Fix requiredObject validator being written on opposite day

--- src/validators/requiredObject.ts
+++ src/validators/requiredObject.ts
@@ -3,8 +3,8 @@ import { Validator, PrimitiveErrors, ObjectTopLevelError } from '../types'
 export function requiredObject(message: string): Validator<object, ObjectTopLevelError<PrimitiveErrors>> {
     return function requiredValidator(value: object | undefined): ObjectTopLevelError<PrimitiveErrors> | undefined {
         if (value === undefined || value === null)
-            return undefined
+            return { _self: [ message ] }
         
-        return { _self: [ message ] }
+        return undefined
     }
 }
