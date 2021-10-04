6fb6d25a94880a31f4883e489aa8fcb91ccc5659
Leon Strauss <me@lionc.de>
2021-10-04 17:04:26 +0200

    :recycle: User std/collections/deep_merge instead of own deepmerge

--- src/validators/all.ts
+++ src/validators/all.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck Deepmerge magic
 
 import { SubjectType, Validator } from "../types.ts";
-import { deepmerge } from "../deepmerge.ts";
+import { deepMerge } from "../deps.ts";
 
 /**
  * Builds a validator function that applies all given validators to a value, merging their results
@@ -286,7 +286,7 @@ export function all(...validators) {
             return cur;
           }
 
-          return deepmerge(acc, cur);
+          return deepMerge(acc, cur);
         },
         undefined,
       );
--- src/vlad.ts
+++ src/vlad.ts
@@ -1,14 +1,14 @@
 export * from "./validators/all.ts";
 export * from "./validators/allItems.ts";
+export * from "./validators/is.ts";
 export * from "./validators/length.ts";
 export * from "./validators/maxItems.ts";
 export * from "./validators/maxLength.ts";
 export * from "./validators/minItems.ts";
 export * from "./validators/minLength.ts";
+export * from "./validators/nullish.ts";
 export * from "./validators/properties.ts";
 export * from "./validators/requiredObject.ts";
 export * from "./validators/requiredPrimitive.ts";
-export * from "./validators/is.ts";
-export * from "./validators/nullish.ts";
 
 export * from "./types.ts";
