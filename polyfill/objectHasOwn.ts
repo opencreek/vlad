44b0d5dac8b3fcb9ce0b88e2aeb66d9a6bdd5023
Leon Strauss <me@lionc.de>
2021-10-20 14:23:05 +0200

    :art: Format

--- polyfill/objectHasOwn.ts
+++ polyfill/objectHasOwn.ts
@@ -1,12 +1,13 @@
 declare global {
-    interface Object {
-        // deno-lint-ignore ban-types
-        hasOwn(subject: object, property: PropertyKey): boolean
-    }
+  interface Object {
+    // deno-lint-ignore ban-types
+    hasOwn(subject: object, property: PropertyKey): boolean;
+  }
 }
 
 if (!Object.hasOwn) {
-    Object.hasOwn = (subject, property) => Object.prototype.hasOwnProperty.call(subject, property);
+  Object.hasOwn = (subject, property) =>
+    Object.prototype.hasOwnProperty.call(subject, property);
 }
 
-export default null
+export default null;
